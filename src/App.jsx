2755dcd2a91418a22937873d36548871a5de0e76
AsimBinNasir <asimbinnasir2906@gmail.com>
2025-09-11 20:03:28 +0600

    Refactor App component layout and add Navbar component; update CSS imports and theme colors

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,10 @@
 import React from 'react'
-
+import Navbar from './components/Navbar'
 const App = () => {
   return (
-    <div className='text-red-500 text-2xl'>App</div>
+    <div className='w-screen h-screen bg-neutral-900 px-4 pt-4 pb-12 sm:px-6 sm:pt-6 sm:pb-20 md:px-28 md:pt-12'>
+      <Navbar />
+    </div>
   )
 }
 
