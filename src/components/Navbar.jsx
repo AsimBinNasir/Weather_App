7d355952902233137e2e95a07c000626ae0be496
AsimBinNasir <asimbinnasir2906@gmail.com>
2025-09-11 20:36:17 +0600

    Add new weather icons and logo SVGs
    - Added icon-storm.webp and icon-sunny.webp for weather representation.
    - Introduced icon-units.svg for unit display in the UI.
    - Added logo.svg for branding purposes.

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,8 +1,21 @@
 import React from 'react'
-
+import Logo from '../assets/Images/logo.svg'
+import IconUnit from '../assets/Images/icon-units.svg'
+import DropDownArrow from '../assets/Images/icon-dropdown.svg'
 const Navbar = () => {
   return (
-    <div>Navbar</div>
+    <div className='w-full h-auto flex items-center justify-between'>
+      <div>
+        <img src={Logo} alt="Weather Now Logo" className='w-auto h-10' />
+      </div>
+      <div>
+        <button className='flex gap-2.5 items-center justify-center px-4 py-3 rounded-lg bg-neutral-700'>
+          <img src={IconUnit} alt="Icon Units Logo"  className='w-4 h-4'/>
+          <span className='font-dmsans text-lg text-neutral-0'>Units</span>
+          <img src={DropDownArrow} alt="Dropdown Arrow" />
+        </button>
+      </div>
+    </div>
   )
 }
 
