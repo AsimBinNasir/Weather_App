241f06a4fa74e5bc36686d2909282b0511c720a8
AsimBinNasir <asimbinnasir2906@gmail.com>
2025-09-12 18:05:48 +0600

    Add Header component to App and update layout styles

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,11 @@
 import React from 'react'
 import Navbar from './components/Navbar'
+import Header from './components/Header'
 const App = () => {
   return (
-    <div className='w-screen h-screen bg-neutral-900 px-4 pt-4 pb-12 sm:px-6 sm:pt-6 sm:pb-20 md:px-28 md:pt-12'>
+    <div className='w-screen h-screen bg-neutral-900 px-4 pt-4 pb-12 sm:px-6 sm:pt-6 sm:pb-20 lg:px-13 lg:pt-7 xl:px-28 xl:pt-12'>
       <Navbar />
+      <Header />
     </div>
   )
 }
---
+++ src/components/Header.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Header = () => {
+  return (
+    <div className='py-16 flex items-center justify-center'>
+        <h2 className='font-dmsans text-neutral-0 text-6xl font-semibold'>How's the Sky Looking Today?</h2>
+    </div>
+  )
+}
+
+export default Header
