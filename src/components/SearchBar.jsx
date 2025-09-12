faca7f56ca078f0ae39c98f54f903a1beb7ef53e
AsimBinNasir <asimbinnasir2906@gmail.com>
2025-09-12 20:10:46 +0600

    Implement SearchBar component with search functionality and styling

--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -1,8 +1,17 @@
 import React from 'react'
+import SearchIcon from '../assets/Images/icon-search.svg'
 
 const SearchBar = () => {
   return (
-    <div>SearchBar</div>
+    <div className='w-fit mx-auto '>
+      <div className='relative flex '>
+      <img src={SearchIcon} alt="Search Icon"  className='absolute left-6 top-1/2 -translate-y-1/2'/>
+      <input 
+      type="text"
+      placeholder='Search for a place'
+      className='h-14 pl-15'/>
+      </div>
+    </div>
   )
 }
 
